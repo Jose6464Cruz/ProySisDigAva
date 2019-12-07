9241e3bcc95a65ee858b3a506a4d7cc772c8242d
Jacc95 <chziming95@gmail.com>
2019-12-07 12:41:14 -0600

    Scanner Working version
    Gets the desired bits in a consistent way. Designed for 4th character of 128 BarCode.

--- Proyecto_final_Scanner_last/Scanner_last.vhd
+++ Proyecto_final_Scanner_last/Scanner_last.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: 		 ITESM QRO
+-- Engineer: 		 Juan Alfonso Chan | Jose Hernandez Cruz
 -- 
 -- Create Date:    18:50:45 12/06/2019 
 -- Design Name: 
@@ -24,8 +24,7 @@ use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
 
 entity Scanner_Last is
-    Port ( Clk             : in  STD_LOGIC;
-           Clk_signal_Scan : in  STD_LOGIC;
+    Port ( Clk_signal_Scan : in  STD_LOGIC;
 			  Rst             : in  std_logic;
            Data_Scan       : in  STD_LOGIC;
            Data_Out        : out  STD_LOGIC_VECTOR (7 downto 0));
@@ -33,28 +32,22 @@ end Scanner_Last;
 
 architecture Behavioral of Scanner_Last is
 signal Reg: STD_LOGIC_VECTOR(10 downto 0) := "00000000000";
-signal cont : integer range 0 to 11 := 0;
---signal regtemp : STD_LOGIC_VECTOR (197 downto 0);
+signal cont : integer range 0 to 119 := 0;
 
 begin
 
-process(Clk,Clk_signal_Scan,cont,rst)
-variable Flag_first : STD_LOGIC := '1';
+process(Clk_signal_Scan,cont,rst)
 begin
    if Rst = '1' then
        Cont <= 0;
 		 Reg <= (others => '0');
-   elsif(falling_edge(Clk) )then
-		if(Clk_signal_Scan = '0' and Flag_first = '1' and cont < 11) then
+   elsif(falling_edge(Clk_signal_Scan) )then
+		if(cont < 119) then
 			Reg <= Data_Scan & Reg(10 downto 1);
 			cont <= cont + 1;
-			Flag_first := '0';
-		elsif(Clk_signal_Scan = '1') then
-			Flag_first := '1';
 	   end if; 
    end if;
 end process; 
-
 
 Data_Out <= Reg(7 downto 0);
 
